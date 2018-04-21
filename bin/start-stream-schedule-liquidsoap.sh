eee754524093680cead74fcae3c0fb81d35c642c
Milan <mc@radiopiloten.de>
2018-04-22 01:49:44 +0200

    add systemd support

--- bin/start-stream-schedule-liquidsoap.sh
+++ bin/start-stream-schedule-liquidsoap.sh
@@ -9,14 +9,14 @@ do
     chown audiostream /var/run/stream-schedule
 
     [ -f /var/run/stream-schedule/stream-schedule-liquidsoap.pid ] && rm /var/run/stream-schedule/stream-schedule-liquidsoap.pid
-    [ -d /var/log/stream-schedule/ ] || mkdir /var/log/stream-schedule/
 
+    [ -d /var/log/stream-schedule/ ] || mkdir /var/log/stream-schedule/
     chmod 755 /var/log/stream-schedule
     chown audiostream:www-data /var/log/stream-schedule/
     [ -f /var/log/stream-schedule/liquidsoap.log ] && chmod 664 /var/log/stream-schedule/liquidsoap.log
     [ -f /var/log/stream-schedule/liquidsoap.log ] && chown audiostream:www-data /var/log/stream-schedule/liquidsoap.log
 
     su -s /bin/sh -c 'exec "$0" "$@" >> /var/log/stream-schedule/liquidsoap.log 2>&1' audiostream -- liquidsoap /etc/stream-schedule/liquidsoap/start.conf
-
+    sleep 3
 done
  
