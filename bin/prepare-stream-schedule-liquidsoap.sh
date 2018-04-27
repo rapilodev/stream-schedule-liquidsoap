5aa883309081c679e6b56e7a9e4c7a115c68a280
Milan <mc@radiopiloten.de>
2018-04-28 01:36:58 +0200

    fix systemd stop and restart, add clocks to icecast

---
+++ bin/prepare-stream-schedule-liquidsoap.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#    pkill -9 -f "/etc/stream-schedule/liquidsoap/start.conf"
+
+[ -d /var/run/stream-schedule ] || mkdir /var/run/stream-schedule
+chmod 775 /var/run/stream-schedule
+chown audiostream /var/run/stream-schedule
+
+[ -f /var/run/stream-schedule/stream-schedule-liquidsoap.pid ] && rm /var/run/stream-schedule/stream-schedule-liquidsoap.pid
+
+[ -d /var/log/stream-schedule/ ] || mkdir /var/log/stream-schedule/
+chmod 755 /var/log/stream-schedule
+chown audiostream:www-data /var/log/stream-schedule/
+[ -f /var/log/stream-schedule/liquidsoap.log ] && chmod 664 /var/log/stream-schedule/liquidsoap.log
+[ -f /var/log/stream-schedule/liquidsoap.log ] && chown audiostream:www-data /var/log/stream-schedule/liquidsoap.log
+
+#    su -s /bin/sh -c 'exec "$0" "$@" >>/var/log/stream-schedule/liquidsoap.log 2>&1' audiostream -- liquidsoap /etc/stream-schedule/liquidsoap/start.conf
+#    sleep 3
+#done
+exit 0
